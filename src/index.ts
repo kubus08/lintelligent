1cd81811d1abae0fd64e04ebed41782e85ad152e
kubao <kuba.olszewski.2060@gmail.com>
2025-11-28 10:02:05 +0100

    v2

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import "dotenv/config"
-import express from "express"
-import { webhookRouter } from "./routes/webhook";
+import express from "express";
 
 const PORT = process.env.PORT || 3000;
-
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-    res.json({ message: "Hello from simple API 🚀! V1" });
+app.get("/", (req, res) => {
+    res.send("GitHub App webhook listener is running!");
 });
-
-app.use("/api/github", webhookRouter);
-
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
